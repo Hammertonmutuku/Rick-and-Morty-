5420f7d74964176f6bc41f1410b25b7012e09e2a
Hammertonmutuku <lthamma3@gmail.com>
2022-04-26 21:30:52 +0300

    added on flutter riverpod dependencies and changed the project to be use  flutter riverpod state managment

--- lib/utilities/widgets/characterWidget.dart
+++ lib/utilities/widgets/characterWidget.dart
@@ -8,36 +8,45 @@ class characterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top:10),
-      child: Row(
-              children: [
-                Image.network(
-                 results.image.toString(),
-                  width: 200,
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Expanded(
+          child: Row(
+                  children: [
+                    Image.network(
+                     results.image.toString(),
+                      width: 200,
+                    ),
+                    Expanded(
+                      child: Container(
+                        
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children:  [
+                         Text(
+                            "${results.name}",
+                            style:
+                                TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                           Text(
+                            "${results.species}",
+                            style:
+                                TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text(
+                             "${results.gender}",
+                            style:
+                                TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  child: Column(
-                    children:  [
-                   Text(
-                      "${results.name}",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                     Text(
-                      "${results.species}",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    Text(
-                       "${results.gender}",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                  ]),
-                ),
-              ],
+        ),
             ),
-          );
+    );
   }
 }
