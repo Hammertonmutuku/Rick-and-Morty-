7c0bfcf266509984f6cc915a512d01a09c6a20b9
Hammertonmutuku <lthamma3@gmail.com>
2022-04-27 10:48:06 +0300

    finalized the project by styling the card

--- lib/utilities/widgets/characterWidget.dart
+++ lib/utilities/widgets/characterWidget.dart
@@ -2,51 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:rick_morty_api/Model/character.dart';
 
 class characterCard extends StatelessWidget {
- final Results results;
+  final Results results;
 
-  const characterCard({ Key? key, required this.results}) : super(key: key);
+  const characterCard({Key? key, required this.results}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      shadowColor: Colors.grey,
+      elevation: 5,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       child: Container(
-        padding: EdgeInsets.all(10),
+        padding:const  EdgeInsets.all(10),
         child: Expanded(
           child: Row(
-                  children: [
-                    Image.network(
-                     results.image.toString(),
-                      width: 200,
-                    ),
-                    Expanded(
-                      child: Container(
-                        
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children:  [
-                         Text(
-                            "${results.name}",
-                            style:
-                                TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                           Text(
-                            "${results.species}",
-                            style:
-                                TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                             "${results.gender}",
-                            style:
-                                TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                        ]),
-                      ),
-                    ),
-                  ],
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image(
+                  image: NetworkImage(
+                      results.image.toString()),
+                      loadingBuilder: (context, child,  progress) {
+                        return progress == null ? child : const LinearProgressIndicator();
+                      },
+                      width: 130,
                 ),
+              ),
+              // CircleAvatar(
+              //    backgroundImage: NetworkImage(results.image.toString()),
+              //    radius: 60,
+              // ),
+              // Image.network(
+              //  results.image.toString(),
+              //   width: 200,
+              // ),
+              Expanded(
+                child: Container(
+                 padding: EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      
+                      children: [
+                        Text(
+                          "${results.name}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          "${results.species}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Text(
+                          "${results.gender}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          ),
         ),
-            ),
+      ),
     );
   }
 }
